3fda96435eec6d3aaafca26b2909ee428b7d686f
Angel Godon <aegodon.24@est.ucab.edu.ve>
2025-05-24 17:15:13 -0400

    usuarios

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import LoginView, DashboardView, LogoutView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', LoginView.as_view(), name='root'), # Redirigir la raíz a la página de login
+]
---
+++ core/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.generic import View # Usaremos Vistas Basadas en Clases (CBV) para orientación a objetos
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoginView(View):
+    plantilla = 'core/login.html'
+
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.plantilla, {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            return redirect('dashboard') 
+        return render(request, self.plantilla, {'form': form})
+
+class DashboardView(LoginRequiredMixin, View):
+    planitlla = 'core/dashboard.html'
+
+    def get(self, request):
+        datos = {
+            'is_admin': request.user.is_staff or request.user.is_superuser,
+            'username': request.user.username,
+        }
+        return render(request, self.template_name, datos)
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login') # Redirige a la página de login después del logout
